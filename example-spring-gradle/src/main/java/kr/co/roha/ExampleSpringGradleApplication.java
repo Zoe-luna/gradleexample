c0530d2b94e69d39a559896bbdf869672ac6dfb6
woan2 <woan2@DESKTOP-40IQ20Q>
2021-07-07 20:07:47 +0900

    create

---
+++ example-spring-gradle/src/main/java/kr/co/roha/ExampleSpringGradleApplication.java
@@ -0,0 +1,13 @@
+package kr.co.roha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleSpringGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleSpringGradleApplication.class, args);
+	}
+
+}
---
+++ example-spring-gradle/src/test/java/kr/co/roha/ExampleSpringGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.roha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleSpringGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
